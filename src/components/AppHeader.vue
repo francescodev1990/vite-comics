bcf8d34dc6de33d9b7b716faa1e578cfa432c35c
Your Name <almannofrancesco@gmail.com>
2023-01-16 15:55:16 +0100

    import AppHeader export AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    
+}
+</script>
+<template lang="">
+    <div>
+        <header>
+            ciao
+        </header>
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
