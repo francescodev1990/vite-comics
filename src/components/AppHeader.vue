9a1acceac7fa8194408788dbefa08e0f462816a7
Your Name <almannofrancesco@gmail.com>
2023-01-17 17:08:33 +0100

    ul li created

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,61 @@
 <script>
 export default {
+    data() {
+        return {
+            menu: [
+                {
+                    label: 'Personaggi',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'Cartoni',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'Film',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'TV',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'Giochi',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'Collezionabili',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'Video',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'Fans',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'News',
+                    url: '#',
+                    active: false
+                },
+                {
+                    label: 'Shop',
+                    url: '#',
+                    active: false
+                },
+            ]
+        }
+    },
     
 }
 </script>
@@ -8,9 +64,20 @@ export default {
         <a href="/" class="brand">
             <img src="../assets/images/dc-logo.png">
         </a>
+        <nav>
+            <ul>
+                <li v-for="item in menu">
+                    <a :href="item.url" :class="item.active ? 'active' :''"></a>
+
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
-<style lang="">
-    
+<style lang="scss">
+    header{
+        text-align: center;
+        margin-top: 25px;
+    }
 </style>
