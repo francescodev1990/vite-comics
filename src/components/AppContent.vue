7dbfd9aa812780f49972432831d32ef361172a98
Your Name <almannofrancesco@gmail.com>
2023-01-18 10:12:31 +0100

    nav digital added

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -4,11 +4,53 @@ export default {
 }
 </script>
 <template lang="">
-    <div>
-        
+    <div class="space_black">
+    <h6>Content goes here</h6>
+    </div>
+    <div class="nav_icon">
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">DIGITAL COMICS</a> 
+                </li>
+                <li>
+                    <a href="#">DC MERCHANDISE</a> 
+                </li>
+                <li>
+                   <a href="#">SUBSCRIPTION</a> 
+                </li>
+                <li>
+                    <a href="#">COMIC SHOP LOCATOR</a> 
+                </li>
+                <li>
+                    <a href="#">DC POWER VISA</a> 
+                </li>
+            </ul>
+        </nav>
     </div>
 </template>
 
-<style lang="">
-    
+<style lang="scss">
+    .space_black{
+        width: 100%;
+        height: 125px;
+        background-color: black;
+    }
+    h6{
+        color: white;
+        font-size: 25px;
+        padding: 50px;
+    }
+    .nav_icon{
+        display: flex;
+        justify-content: center;
+        padding: 25px;
+        width: 100%;
+        height: 150px;
+        background-color: #0282f9;
+    }
+    ul li a{
+        padding: 15px;
+        color: white;
+    }
 </style>
